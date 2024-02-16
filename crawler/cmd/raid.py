bb24f63edef80ff9152a1a532372fcd4a6fb5ea6
67au <oyy6745@gmail.com>
2024-02-17 00:02:09 +0800

    Revert feat(cli/raid): group by tier

--- crawler/cmd/raid.py
+++ crawler/cmd/raid.py
@@ -46,15 +46,13 @@ def run(data_dir: Path, output: str = None, generate: bool = False):
         with open(data_dir.joinpath(f'{lang}/raids.json'), 'r') as fp:
             d = json.load(fp)
             for item in d:
-                if raid.get(item['tier']) is None:
-                    raid[item['tier']] = {}
-                if raid[item['tier']].get(item['id']) is None:
-                    raid[item['tier']][item['id']] = {'name': {}}
-                raid[item['tier']][item['id']]['name'][lang] = item['name']
-                if raid[item['tier']][item['id']].get('hp') is None:
-                    # raid[item['tier']][item['id']]['tier'] = int(item['tier'])
-                    raid[item['tier']][item['id']]['hp'] = int(item['hp'])
-                    raid[item['tier']][item['id']]['icon'] = item['icon']
+                if raid.get(item['id']) is None:
+                    raid[item['id']] = {'name': {}}
+                raid[item['id']]['name'][lang] = item['name']
+                if raid[item['id']].get('hp') is None:
+                    raid[item['id']]['tier'] = int(item['tier'])
+                    raid[item['id']]['hp'] = int(item['hp'])
+                    raid[item['id']]['icon'] = item['icon']
 
     with open(output, 'w', encoding='utf-8') as fp:
         json.dump(raid, fp, indent=4, ensure_ascii=False, sort_keys=True)

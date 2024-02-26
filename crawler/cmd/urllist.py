2af84bf6f31ccfac3813e954060a5986fe2e9f05
67au <oyy6745@gmail.com>
2024-02-26 15:02:40 +0800

    feat(cmd): add urllist module

---
+++ crawler/cmd/urllist.py
@@ -0,0 +1,27 @@
+
+
+import heapq
+import json
+from pathlib import Path
+from crawler.spiders import bosses, classes, followers, items, monsters, raids, spells
+
+
+crawlers = [
+    bosses, classes, followers, items,
+    monsters, raids, spells
+]
+
+base_lang = 'en'
+
+def run(data_dir: Path, output: str = None, generate: bool = False, target: str = None):
+    index_dir = data_dir.joinpath('index')
+    if output:
+        urls = []
+        for crawler in crawlers:
+            key = crawler.CodexSpider.name
+            with open(index_dir.joinpath(base_lang, f'{key}.json')) as f:
+                items = json.load(f)
+                for item in items:
+                    heapq.heappush(urls, f'/codex/{item["category"]}/{item["id"]}/')
+        with open(output, 'w') as f:
+            f.writelines(f'{heapq.heappop(urls)}\n' for _ in range(len(urls)))
